1d1f99053d9a88fa7d5f87fc5e51bc6d90c06601
Filb68 <bistro.@free.fr>
2023-02-21 12:15:06 +0100

    Envoi 12h15

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
+  get 'static_pages/secret'
   devise_for :users
+
+  root "static_pages#index" 
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
